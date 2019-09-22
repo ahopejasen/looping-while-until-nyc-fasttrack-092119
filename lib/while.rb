1730f7c80a8043e4f26bf44ecde360031dbcdc2c
punctual-enhancer-1584 <amal.jasen@gmail.com>
2019-09-22 16:15:24 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,15 @@
 def using_while
   levitation_force = 6
+<<<<<<< HEAD
   spell_name="Wingardium Leviosa"
   
   while levitation_force< 10 do
     puts spell_name
     levitation_force +=1 
   end
+=======
+  spell_name=
+>>>>>>> 9b317e36e62e53d306b994bfffed152db3edfd0d
 	
   #your code here
 end
